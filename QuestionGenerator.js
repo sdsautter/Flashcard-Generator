db820af8d875be2de8a0dd6b406e3b6920a916d0
sdsautter <sdsautter@gmail.com>
2017-05-03 17:02:59 -0400

    Deleted a file I didn't need

--- QuestionGenerator.js
+++
@@ -1,9 +0,0 @@
-var BasicCard = require("./BasicCard.js");
-
-var basicArray = [];
-var clozeArray = ["poop"];
-
-var cheese = new BasicCard("Cheese", "Delicious and made of milk.");
-basicArray.push(cheese);
-
-module.exports = basicArray;
