e0c7fb8c5142cd35677a80581c2ded9c8ef97d6d
sdsautter <sdsautter@gmail.com>
2017-05-03 17:00:56 -0400

    Took out some commented out code that I wasn't using

--- QuestionGenerator.js
+++ QuestionGenerator.js
@@ -6,7 +6,4 @@ var clozeArray = ["poop"];
 var cheese = new BasicCard("Cheese", "Delicious and made of milk.");
 basicArray.push(cheese);
 
-// console.log(basicArray); 
-
 module.exports = basicArray;
-// module.exports = clozeArray;
